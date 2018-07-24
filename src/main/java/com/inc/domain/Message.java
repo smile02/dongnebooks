5e4ff6ae0952f401a97c0951a60bf648bc121241
ko hyeonjin <gosu12002@naver.com>
2018-07-24 19:00:46 +0900

    maven배포테스트, 채팅방 적용 중

---
+++ src/main/java/com/inc/domain/Message.java
@@ -0,0 +1,31 @@
+package com.inc.domain;
+
+public class Message {
+	private String to, from, msg;
+
+	public String getTo() {
+		return to;
+	}
+
+	public void setTo(String to) {
+		this.to = to;
+	}
+
+	public String getFrom() {
+		return from;
+	}
+
+	public void setFrom(String from) {
+		this.from = from;
+	}
+
+	public String getMsg() {
+		return msg;
+	}
+
+	public void setMsg(String msg) {
+		this.msg = msg;
+	}
+	
+	
+}
