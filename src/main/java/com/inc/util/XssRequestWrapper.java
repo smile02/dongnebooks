f3a98e194f2bc125e627209661f94a7c68c8a750
Lee Hana <quacia@naver.com>
2018-07-24 16:23:54 +0900

    XSS 방어 필터 추가

---
+++ src/main/java/com/inc/util/XssRequestWrapper.java
@@ -0,0 +1,54 @@
+package com.inc.util;
+
+import java.io.IOException;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletRequestWrapper;
+
+public class XssRequestWrapper extends HttpServletRequestWrapper {
+
+	public XssRequestWrapper(HttpServletRequest request) throws IOException{
+		super(request);
+		
+	}
+
+	@Override
+	public String getParameter(String parameter) {
+		String value = super.getParameter(parameter);
+		if(value == null ) {
+			return null;
+		}else {
+			return cleanXSS(value);
+		}
+		
+	}
+
+	@Override
+	public String[] getParameterValues(String parameter) {
+		String[] values = super.getParameterValues(parameter);
+		if(values == null) {
+			return null;
+		}
+		
+		String[] encodedValues = new String[values.length];
+		for(int i = 0; i <values.length ; i++) {
+			encodedValues[i] = cleanXSS(values[i]);
+			System.out.println(encodedValues[i]);
+		}
+		
+		return encodedValues;
+	}
+	
+	private String cleanXSS(String value) {
+		value = value.replaceAll("<", "&lt;");
+		value = value.replaceAll(">", "&gt;");
+		value = value.replaceAll("eval\\((.*)\\)","");
+		value = value.replaceAll("\\(", "&#40;");
+		value = value.replaceAll("\\)", "&#41;");
+		value = value.replaceAll("'", "&#39;");
+		value = value.replaceAll("[\\\"\\\'][\\s]*javascript:(.*)[\\\"\\\']", "\"\"");
+		value = value.replaceAll("script", "");
+		return value;
+	}
+
+}
